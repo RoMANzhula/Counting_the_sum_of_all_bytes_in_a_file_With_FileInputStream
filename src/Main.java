b00ed32384b1eb0d1e517bb2aaf8a47f1d0edb10
RoMANzhula <roman1985al@ukr.net>
2023-04-18 19:23:55 +0300

    Initial commit

--- src/Main.java
+++ src/Main.java
@@ -1,12 +1,23 @@
+import java.io.BufferedReader;
 import java.io.FileInputStream;
 import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.Collections;
 
 public class Main {
     public static void main(String[] args) {
-        String fileName = "pathToFile"; //создаем строковую переменную и инициализируем ее адресом(путем к) нашего файла, в
+        String fileName = "C:\\Users\\Admin\\Desktop\\Diploma\\Technologies.txt"; //создаем строковую переменную и инициализируем ее адресом(путем к) нашего файла, в
         //котором мы хотим посчитать байты
+        sumOfAllBytesInFile(fileName);
+        searchMaximumByte(fileName);
+        searchMinimumByte(fileName);
+    }
+
+    public static void sumOfAllBytesInFile(String pathToFile) { //метод для подсчета всех байтов в указанном файле
+
         long containerForBites = 0; //создаем 64-битный контейнер для подсчета байтов
-        try (FileInputStream fileInputStream = new FileInputStream(fileName)) {//создаем поток для последовательного ввода данных
+        try (FileInputStream fileInputStream = new FileInputStream(pathToFile)) {//создаем поток для последовательного ввода данных
             //из указанного файла в блоке try-with-resource(который в свою очередь автоматически освобождает ресурсы, закрывая потоки)
 
             while (fileInputStream.available() > 0) { //будет выполнять цикл, пока в потоке для чтения есть еще байты
@@ -14,10 +25,47 @@ public class Main {
                 containerForBites += dataForRead; //добавляем считанный байт в наш контейнер байтов
             }
 
-        } catch (IOException ioException) {
-            ioException.printStackTrace();
+        } catch (IOException ioException) { //ловим исключение для входного/выходного потока
+            ioException.printStackTrace(); //выводим исключение в стек
         }
-        System.out.println("The sum of all bytes in file: " + fileName + " = " + containerForBites);
+        System.out.println("The sum of all bytes in your file: " + containerForBites); //выводим в консоль данные
+    }
 
+    public static void searchMaximumByte(String pathToFile) { //метод для поиска максимального байта в указанном файле
+        ArrayList<Long> arrayList = new ArrayList<>(); //создаем коллекцию типа интерфейса ArrayList<Long> для хранения байтов,
+        //прочитанных из указанного файла
+
+        try (FileInputStream fileInputStream = new FileInputStream(pathToFile)) { //создаем поток для последовательного ввода данных
+            //из указанного файла в блоке try-with-resource(который в свою очередь автоматически освобождает ресурсы, закрывая потоки)
+            while (fileInputStream.available() > 0) { //запускаем цикл пока(есть еще непрочитанные байты)
+                long containerForBytes = fileInputStream.read();//создаем контейнер для байтов в виде переменной и читаем
+                // очередной байт в данную переменную
+                arrayList.add(containerForBytes);//заполняем список байтами
+            }
+            System.out.println("This is the maximum byte in your file: " + Collections.max(arrayList));//выводим в консоль -
+            // функция super-класс Коллекции - вывести максимальное число(из коллекции)
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static void searchMinimumByte(String pathToFile) { //метод для поиска минимального байта в указанном файле
+        ArrayList<Long> arrayList = new ArrayList<>(); //создаем коллекцию типа интерфейса ArrayList<Long> для хранения байтов,
+        //прочитанных из указанного файла
+
+        try (FileInputStream fileInputStream = new FileInputStream(pathToFile)) { //создаем поток для последовательного ввода данных
+            //из указанного файла в блоке try-with-resource(который в свою очередь автоматически освобождает ресурсы, закрывая потоки)
+            while (fileInputStream.available() > 0) { //запускаем цикл пока(есть еще непрочитанные байты)
+                long containerForBytes = fileInputStream.read();//создаем контейнер для байтов в виде переменной и читаем
+                // очередной байт в данную переменную
+                arrayList.add(containerForBytes);//заполняем список байтами
+            }
+            System.out.println("This is the minimum byte in your file: " + Collections.min(arrayList));//выводим в консоль -
+            // функция super-класс Коллекции - вывести минимальное число(из коллекции)
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 }
